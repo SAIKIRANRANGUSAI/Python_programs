511beb4d4f03004ede0df307c6065ef004b5223f
SAIKIRANRANGUSAI <saikiranrangu6@gmail.com>
2023-09-13 15:55:23 +0530

    creating a list

---
+++ creating_list.py
@@ -0,0 +1,6 @@
+empty_list = []
+sai = int(input("enter how many you want to store in a list:"))
+for i in range(sai):
+    list_is = input("enter the values")
+    empty_list.append(list_is)
+print(empty_list)
